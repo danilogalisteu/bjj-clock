db9561f03de5e830ebc75e2f85f4c04e39da2771
Danilo Galisteu <danilogalisteu@gmail.com>
2025-03-24 23:07:19 -0300

    clock component

---
+++ src/lib/Clock.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { formatTimeHHMMSS, formatTimeYYMMDD } from '$lib/datetime';
+
+	let time = $state(new Date());
+
+	onMount(() => {
+		const interval = setInterval(() => {
+			time = new Date();
+		}, 1000);
+
+		return () => {
+			clearInterval(interval);
+		};
+	});
+</script>
+
+<time datetime={time.toISOString()}>
+	<div class="time">{formatTimeHHMMSS(time)}</div>
+	<div class="date">{formatTimeYYMMDD(time)}</div>
+</time>
+
+<style>
+	time {
+		display: block;
+		font-family: Arial;
+		margin-bottom: 0.2em;
+	}
+	.time {
+		font-size: 4em;
+	}
+	.date {
+		font-size: 3em;
+	}
+</style>
