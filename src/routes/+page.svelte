7241fab5169e9678e34406e6e542121b0574ee3a
Danilo Galisteu <danilogalisteu@gmail.com>
2025-03-26 20:52:24 -0300

    moved component files

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script>
-	import Clock from '$lib/Clock.svelte';
-	import Timer from '$lib/Timer.svelte';
+	import Clock from '$lib/components/Clock.svelte';
+	import Timer from '$lib/components/Timer.svelte';
 </script>
 
 <main class="flex flex-col gap-4 items-center">
